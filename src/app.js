1ce56ffb360c570fe4da715aefba39096168d169
Thiago <thiagohenrique.sousa321@gmail.com>
2022-05-29 06:32:37 -0300

    finalizando o projeto

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const user = require("../models/User")
+const bcrypt = require("bcrypt")
+const jwtSecret = "GitHub"
+const jwt = require("jsonwebtoken")
+const { hash } = require("bcrypt")
+
 
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
 
 
 mongoose.connect("mongodb://localhost:27017/guiapics", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,19 +32,81 @@ app.post("/user", async (req, res) => {
 
         let { name, email, password } = req.body
 
-        let objectUser = {
-            name,
-            email,
-            password
+        let userEmail = await User.findOne({email: email})
+            
+            if(userEmail != undefined){
+               
+                res.statusCode = 400
+                res.json({error:"Email já cadastrado!"})
+                return
+            }
+
+
+        if(name.trim() == "" || email.trim() == "" || password.trim() == ""){
+            res.sendStatus(400)
+            return        
         }
 
-        let newUser = await new User(objectUser)
-        newUser.save()
-        res.json({email: req.body.email})
-    } catch (err) {
+            let salt = await bcrypt.genSalt(10)
+            let hash = await bcrypt.hash(password,salt)
+
+            let objectUser = {
+                name,
+                email,
+                password: hash
+            }
+
+            let newUser = new User(objectUser)
+            
+            await newUser.save()
+            res.json({email: email})
+
+    }catch(err){
         res.sendStatus(500)
     }
+})
 
+app.delete("/user/:email", async (req,res) => {
+    await User.deleteOne({email:req.params.email})
+    res.sendStatus(200)         
+})
+
+app.post("/auth", async (req, res) => {
+    let { email, password} = req.body
+
+    
+    let user = await User.findOne({email:email})
+
+    if(user == undefined) {
+        res.statusCode = 403
+        res.json({
+            errors:{
+                email:"E-mail não cadastrado"
+            }})
+        return
+    }
+
+    let isPasswordRight = await bcrypt.compare(password,user.password)
+
+    if(!isPasswordRight){
+        console.log("olaa")
+        res.statusCode = 403
+        res.json({
+            errors:{
+                pass:"Senha incorreta"
+            }})
+        return
+    }
+
+    jwt.sign({email},jwtSecret,{expiresIn:"48h"},(err, token) => {
+        
+        if(err){
+            console.log(err)
+            res.sendStatus(500)
+        }else{
+            res.json({token})
+        }
+    })
 })
 
 
--- test/user.test.js
+++ test/user.test.js
@@ -2,6 +2,29 @@ const app = require("../src/app")
 const superTest = require("supertest")
 const request = superTest(app)
 
+const mailUser = {name: "TH", email:"thiago@gmail.com", password:"2323"}
+const mailUserLogin = {email:"thiago@gmail.com", password:"2323"}
+const aleatoryMail = {email:"emailAleatorio@gmail.com", password:"2323"}
+const mailUserWithWrongPassword = {email:"thiago@gmail.com", password:"senhaErrada"}
+
+
+beforeAll( () => {
+    return request.post("/user")
+    .send(mailUser)
+    .then(res => {})
+    .catch(err => {
+    console.log(err)
+    })
+})
+
+afterAll( () => {
+
+    return request.delete(`/user/${mailUser.email}`)
+    .then(res => {
+    })
+    .catch(res => {console.log(res)})
+})
+
 
 
 describe("Cadastro de usuário",() => {
@@ -23,5 +46,75 @@ describe("Cadastro de usuário",() => {
             })
         })
 
+        
+        test("Deve impedir que um usuário se cadastre com os dados vazios", () => {
 
+            let user = {name:"", email:"", password:""}
+
+            return request.post("/user").send(user).then(res => {
+
+                expect(res.statusCode).toEqual(400)
+
+            }).catch(err => {
+                fail(err)
+            })
+        })
+
+        test("Deve impedir que um usuário se cadastre com o email repetido", () => {
+
+            let user = {name: "fulano", email:mailUser.email, password:"2323"}
+
+                return request.post("/user").send(user).then(res => {
+                   
+                    expect(res.body.error).toEqual("Email já cadastrado!")
+                    expect(res.statusCode).toEqual(400)
+                }).catch(err => {
+                    console.log(err)
+                    fail(err)    
+                })
+        })
 })
+
+describe("Autenticação", () => {
+
+    test("Deve retornar o token jwt", () => {
+        return request.post("/auth").send(mailUserLogin)
+        .then(res => {
+            expect(res.statusCode).toEqual(200)
+            expect(res.body.token).toBeDefined()
+        })
+        .catch(err => {
+            console.log(err)
+            fail(err)
+        })
+    })
+
+    test("Deve impedir que um usuário não cadastrado se logue", () => {
+        return request.post("/auth").send(aleatoryMail)
+        .then(res => {
+            expect(res.statusCode).toEqual(403)
+            expect(res.body.errors.email).toEqual("E-mail não cadastrado")
+        })
+        .catch(err => {
+            console.log(err)
+            fail(err)
+        })
+    })
+
+    test("Deve impedir que um usuário com a senha errada não se logue", () => {
+        return request.post("/auth").send(mailUserWithWrongPassword)
+        .then(res => {
+            expect(res.statusCode).toEqual(403)
+            expect(res.body.errors.pass).toEqual("Senha incorreta")
+        })
+        .catch(err => {
+            console.log(err)
+            fail(err)
+        })
+    })
+})
+
+
+
+
+
